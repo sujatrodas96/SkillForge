f8c878624f1746908e52263f0045f7e23b107510
sujatro das <sujatro.das96@gmail.com>
2025-11-05 01:33:50 +0530

    resturcture jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,48 @@
+# Security Group for SkillForge EC2
+resource "aws_security_group" "skillforge_sg" {
+  name        = "skillforge-sg"
+  description = "Allow SSH and HTTP access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "skillforge-sg"
+  }
+}
+
+# Get default VPC for security group
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Attach this SG to your instance
 resource "aws_instance" "skillforge_app" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t3.micro"
   key_name      = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.skillforge_sg.id]
 
   tags = {
     Name = "SkillForge"
@@ -12,6 +53,6 @@ resource "aws_instance" "skillforge_app" {
     sudo apt update -y
     sudo apt install -y docker.io
     sudo systemctl start docker
-    sudo docker run -d -p 80:80 ${var.docker_image}
+    sudo docker run -d -p 80:80 sujatro123/skillforge:latest
   EOF
 }
